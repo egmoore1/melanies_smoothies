6cd2b08c32ea591389cfedc83cbb6cea97063391
egmoore1 <erick.moore@dpi.nc.gov>
2025-04-08 11:12:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ if time_to_insert:
 # New section to display smoothiefroot nutrition information 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
     st.success('Your Smoothie is ordered!'+name_on_order, icon="✅");
