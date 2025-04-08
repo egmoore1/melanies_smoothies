915f0f16b189c29b1d22ebbc2a1c9e1088596c36
egmoore1 <erick.moore@dpi.nc.gov>
2025-04-08 11:16:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    st.success('Your Smoothie is ordered!'+name_on_order, icon="✅");
+st.success('Your Smoothie is ordered!'+name_on_order, icon="✅");
